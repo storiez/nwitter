e9a051a1f56043adbb7f08cac423e8faf97463d1
Bruce.Cheong <storiez@naver.com>
2021-09-17 21:08:20 +0900

    로그인/사용차 추가 toggle 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -7,6 +7,7 @@ const Auth = () => {
     const [password, setPassword] = useState("");
     // eslint-disable-next-line no-unused-vars
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) => {
         const {
@@ -36,9 +37,13 @@ const Auth = () => {
                 await fbaseSignInWithEmailAndPassword(email, password);
             }
         } catch (error) {
-            console.log("error login onSubmit : " , error);
+            setError(error.message);
         }
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
+
 
     return (
         <div>
@@ -64,7 +69,11 @@ const Auth = () => {
                     maxLength="20"
                 />
                 <input type="submit" value={newAccount ? "Create Account" : "log In"} />
+                {error ? error : null}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
